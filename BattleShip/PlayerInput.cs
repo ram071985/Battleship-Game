bc7765dde3903bdc5fdc02f140670f7ee4149d40
ram071985 <reidmuchow@gmail.com>
2020-05-12 18:13:42 -0500

    throwing exception error.

--- BattleShip/PlayerInput.cs
+++ BattleShip/PlayerInput.cs
@@ -8,25 +8,35 @@ namespace BattleShip
         {
             Console.WriteLine("Choose a grid row number between 1 and 10");
             var isNumber = int.TryParse(Console.ReadLine(), out int userInput);
-            if (isNumber == false)
-            {
-                while (userInput < 1 || userInput > 10 && isNumber == false)
+            if (userInput < 1 || userInput > 10)
+                while (isNumber == false)
+                {
                     Console.WriteLine("INVALID ENTRY!");
-                Console.WriteLine("PLEASE ENTER A VALID NUMBER 1-10");
-                userInput = Convert.ToInt32(Console.ReadLine());
-            }
+                    Console.WriteLine("PLEASE ENTER A VALID NUMBER 1-10");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                }
             return userInput;
-
         }
 
         public int ReadPlayerColumnInput()
         {
             Console.WriteLine("Choose a grid column number between 1 and 10");
             var isNumber = int.TryParse(Console.ReadLine(), out int userInput);
-            if (isNumber == false)
+            while (isNumber == false)
             {
-                while (userInput < 1 || userInput > 10 && isNumber == false)
-                    Console.WriteLine("INVALID ENTRY!");
+                Console.WriteLine("INVALID ENTRY!");
+                Console.WriteLine("PLEASE ENTER A VALID NUMBER 1-10");
+         
+                if (userInput >= 1 || userInput <= 10)
+                {
+                    return userInput;
+                }
+            }
+
+
+            while (userInput < 1 || userInput > 10)
+            {
+                Console.WriteLine("INVALID ENTRY!");
                 Console.WriteLine("PLEASE ENTER A VALID NUMBER 1-10");
                 userInput = Convert.ToInt32(Console.ReadLine());
             }
