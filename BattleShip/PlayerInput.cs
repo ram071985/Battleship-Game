0f5286fe3ff30b9a5a676e14dff67f54b4cf56f2
ram071985 <reidmuchow@gmail.com>
2020-05-13 20:57:48 -0500

    new add

--- BattleShip/PlayerInput.cs
+++ BattleShip/PlayerInput.cs
@@ -8,12 +8,12 @@ namespace BattleShip
         {
             Console.WriteLine("Choose a grid row number between 1 and 10");
             var isNumber = int.TryParse(Console.ReadLine(), out int userInput);
-            if (userInput < 1 || userInput > 10)
+            if (userInput < 1 || userInput > 10 || isNumber == false)
                 while (isNumber == false)
                 {
                     Console.WriteLine("INVALID ENTRY!");
                     Console.WriteLine("PLEASE ENTER A VALID NUMBER 1-10");
-                    userInput = Convert.ToInt32(Console.ReadLine());
+                    isNumber = int.TryParse(Console.ReadLine(), userInput);
                 }
             return userInput;
         }
