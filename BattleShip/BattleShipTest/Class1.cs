b7aa0201b7fc79d209997e8dd3bff7c701e71e74
ram071985 <reidmuchow@gmail.com>
2020-05-17 12:46:43 -0500

    remade Battlship.Tests

--- BattleShip/BattleShipTest/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BattleShipTest
-{
-    public class Class1
-    {
-    }
-}
---
+++ BattleShip/Battleship.Tests/GameLogicTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BattleShip;
+using NUnit.Framework;
+
+namespace Battleship.Tests
+
+{
+    [TestFixture]
+    public class GameLogicTests
+    {
+        private GameLogic _gameLogic;
+
+        public void Setup()
+        {
+            var gameGrid = new GameGrid();
+            var playerInput = new PlayerInput();
+
+            _gameLogic = new GameLogic(gameGrid, playerInput);
+        }
+
+        [Test]
+        public void should_is_game_over_due_to_depleted_missiles_return_true()
+        {
+            _gameLogic.roundNumber = 8;
+            var result = _gameLogic.IsGameOverDueToDepletedMissiles();
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void should_is_game_over_due_to_depleted_missiles_return_false()
+        {
+            _gameLogic.roundNumber = 0;
+            var result = _gameLogic.IsGameOverDueToDepletedMissiles();
+            Assert.IsFalse(result);
+        }
+    }
+}
