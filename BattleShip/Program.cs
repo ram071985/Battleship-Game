7025de9652de6345e7028d218de4bb7bd69030ac
ram071985 <reidmuchow@gmail.com>
2020-07-02 10:03:58 -0500

    fixed merge issues with VSC.

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-
-namespace BattleShip
-{
-    class Program
-    {
-        static int shipRow;
-        static int shipColumn;
-        static int playerRow;
-        static int playerColumn;
-        static int roundNumber;
-        static int battleshipHits;
-
-            static void Main(string[] args)
-        {
-            StartGame();
-            GenerateNewRound();
-         
-=======
-﻿using System;
+﻿﻿using System;
 
 namespace BattleShip
 {
@@ -27,82 +7,8 @@ namespace BattleShip
         private static GameGrid _gameGrid;
         private static GameLogic _gameLogic;
         private static PlayerInput _playerInput;
->>>>>>> week-three
         
 
-<<<<<<< HEAD
-
-
-        public static void BattleshipPosition()
-        {
-            Random randomBattleshipRow = new Random();
-            shipRow = randomBattleshipRow.Next(1, 11);
-            Random randomBattleshipColumn = new Random();
-            shipColumn = randomBattleshipColumn.Next(1, 11);
-        }
-
-        static void CalculateHit()
-        {
-
-           if (isGameOverDueToDepletedMissiles())
-            {
-                Console.WriteLine("Missiles Depleted");
-                Console.WriteLine();
-                Console.WriteLine("GAME OVER");
-                Environment.Exit(0);
-            } else if(isBattleshipHit())
-            {
-                battleshipHits += 1;
-                Console.WriteLine(battleshipHits);
-                Console.WriteLine("You've hit the battleship!");
-                Console.WriteLine();
-            
-            } else if(isBattleshipHitWithFiveMissiles())
-            {
-                Console.WriteLine("CONGRATULATIONS! You've sunk the battleship!");
-                Console.WriteLine();
-                Console.WriteLine("You've Won The Game!");
-            }
-            else
-            {
-                Console.WriteLine("You missed the Battleship!");
-            }
-        }
-
-        static bool isBattleshipHitWithFiveMissiles()
-        {
-            if (battleshipHits == 5)
-            {
-                return true;
-
-                
-            } else
-            {
-                return false;
-            }
-        }
-
-        static bool isBattleshipHit()
-        {
-            if (playerRow == shipRow && playerColumn == shipColumn)
-                return true;
-            else
-            {
-                return false;
-            }
-        }
-
-        static bool isGameOverDueToDepletedMissiles()
-        {
-            if (playerRow != shipRow && playerColumn != shipColumn && roundNumber == 9)
-            {
-                return true;
-            } else
-
-            {
-                return false;
-            }
-=======
         static void Main(string[] args)
         {
             _gameGrid = new GameGrid();
@@ -110,39 +16,12 @@ namespace BattleShip
             _gameLogic = new GameLogic(_gameGrid, _playerInput);
          
             StartGame();           
->>>>>>> week-three
         }
 
         public static void StartGame()
         {
             Console.WriteLine("Welcome to Battleship!");
             Console.WriteLine();
-<<<<<<< HEAD
-            Console.WriteLine("You get 8 attempts to fire at the battleship on the grid.  5 hits will sink the battleship.");
-            Console.WriteLine();
-
-
-        }
-
-        static void GenerateNewRound()
-        {
-            for (int i = 1; i < 9; i++)
-            {
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("Round " + i + " of 8");
-                roundNumber = i;
-                Console.WriteLine(roundNumber);
-                ChooseRowNumber();
-                ChooseColumnNumber();
-                FireAtBattleShip();
-                CalculateHit();
-            }
-        }
-
-
-        public static void ChooseRowNumber()
-=======
             Console.WriteLine("You have 8 attempts to fire at the battleship on the grid.  5 hits will sink the battleship.");
             Console.WriteLine();
             GenerateNewRound();
@@ -150,7 +29,6 @@ namespace BattleShip
         }
 
         static void GenerateNewRound()
->>>>>>> week-three
         {
             for (int i = 1; i < 9; i++)
             {
@@ -176,12 +54,9 @@ namespace BattleShip
 
         }
 
-<<<<<<< HEAD
-=======
 
 
     }
->>>>>>> week-three
 
 
 }
